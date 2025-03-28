44eea8fb7a217f083c3854bac82107cd97508751
Christian Lewe <ch_lewe@agdsn.me>
2025-03-28 18:51:17 +0800

    feat: Add fuzz test

---
+++ fuzz/fuzz_targets/stack_transform.rs
@@ -0,0 +1,33 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use bitfony::opcodes::{find_shortest_transformation, StackOp};
+
+fuzz_target!(|input: (&[u8], &[u8])| {
+    let (source_stack, target) = input;
+
+    // Target contains item that is not contained in source stack.
+    // There is no transformation from source to target, so bail out early.
+    if target.iter().any(|x| !source_stack.contains(x)) {
+        return;
+    }
+
+    let state = find_shortest_transformation(source_stack, target)
+        .expect("there should be a transformation");
+    let script = state.reversed_script.into_iter().rev();
+    let mut stack = Vec::from(source_stack);
+    StackOp::apply(&mut stack, script).expect("transformation script should not fail");
+    assert!(
+        state.script_bytes <= target.len() * 2,
+        "maximum transformation cost should be 2 * N for N target items"
+    );
+    assert!(
+        target.len() <= stack.len(),
+        "result stack should be long enough to include target"
+    );
+    assert_eq!(
+        &stack[stack.len() - target.len()..],
+        target,
+        "result stack top should be equal to target"
+    );
+});
