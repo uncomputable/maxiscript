c342b298eb727d528ee201fc556a99ac0706108b
Christian Lewe <ch_lewe@agdsn.me>
2025-03-30 12:19:13 +0800

    test: Parse -> ToString -> Parse roundtrip

---
+++ fuzz/fuzz_targets/parse_display.rs
@@ -0,0 +1,16 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use bitfony::parse_program_string;
+
+fuzz_target!(|src: &str| {
+    let program_string_pass1 = match parse_program_string(src) {
+        Some(x) => x,
+        None => return,
+    };
+    let program_string_pass2 = match parse_program_string(&program_string_pass1) {
+        Some(x) => x,
+        None => return,
+    };
+    assert_eq!(program_string_pass1, program_string_pass2);
+});
